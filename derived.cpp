47a02295b1be8afd686be6d835a8be1b52d862bc
1RaghavM <lebrongoated2323@gmail.com>
2026-02-24 12:38:36 -0500

    Continue

---
+++ derived.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Polygon{
+    protected:
+        int width, height;
+    public:
+        void setValues(int w, int h){width = w; height = h;}
+};
+
+class Rectangle : public Polygon{
+    public:
+        int area(){return(width*height);}
+};
+
+class Triangle : public Polygon{
+    public:
+        int area(){return((width*height)/2);}
+};
+
+int main(){
+    Rectangle rect; Triangle trgl;
+
+    rect.setValues(4, 5);
+    trgl.setValues(4, 5);
+
+    cout << "Rectangle Area: " << rect.area() << endl;
+    cout << "Triangle Area: " << trgl.area() << endl;
+    
+    return 0;
+}
